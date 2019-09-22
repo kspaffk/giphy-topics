cc16e606a0d528c7344d5890220e219106e27bc5
kent <51271998+kspaffk@users.noreply.github.com>
2019-09-21 18:14:48 -0600

    create script to create buttons and populate gifs from giphy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,46 @@
-$document.ready(function() {
+$(document).ready(function() {
     var queryURL = {
-        url: 'https://api.giphy.com/v1/gifs/search?',
-        APIkey: 'api_key=lW5ME5ZTVc4hD1XPWWNITuRBE0z1xaPp',
+        url: 'https://api.giphy.com/v1/gifs/search?api_key=lW5ME5ZTVc4hD1XPWWNITuRBE0z1xaPp',
+        limit: '&limit=10',
+        rating: '&rating=pg',
         getSearch: function(term) {
-            search: '&q' + term;
+            var search = '&q=' + term;
             return search;
         }
     }
 
-    var topicList = ["halloween", "fall", "beagle", ""]
+    var topicList = ["halloween", "fall", "beagle", "computers", "programming"];
+
+    function createButtons() {
+        topicList.forEach(function(t,i){
+            var btn = $("<button>").addClass("gif-btn");
+            btn.attr("data-index", i).text(t);
+            $("header").append(btn);
+        });
+    }
+
+    function clickButtons() {
+        var btnIndex = $(this).attr("data-index");
+
+        $.ajax({
+            url: queryURL.url + queryURL.getSearch(topicList[btnIndex]) + queryURL.limit + queryURL.rating,
+            method: "GET"
+        }).then(createGifs);
+    }
+
+    function createGifs(gifData) {
+        console.log(gifData);
+        for(var i = 0; i < gifData.data.length; i++) {
+            var gifDataItem = gifData.data[i];
+            var imgContainer = $('<div>').addClass('img-container');
+            var img = $('<img>').attr('src', gifDataItem.images.fixed_height_still.url);
+            imgContainer.append(img);
+            $("section").append(imgContainer);
+        }
+    }
+
+
+    
+    createButtons();
+    $(".gif-btn").on('click', clickButtons);
 });
