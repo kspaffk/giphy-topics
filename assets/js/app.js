521efcd48df67352d368f128c0ac7607b534817b
kent <51271998+kspaffk@users.noreply.github.com>
2019-09-25 00:49:47 -0600

    add functionality to not allow same topic to be entered

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,89 +1,106 @@
 $(document).ready(function() {
-    
     var queryURL = {
-        url: 'https://api.giphy.com/v1/gifs/search?api_key=lW5ME5ZTVc4hD1XPWWNITuRBE0z1xaPp',
-        limit: '&limit=10',
-        rating: '&rating=pg',
+        url:
+            "https://api.giphy.com/v1/gifs/search?api_key=lW5ME5ZTVc4hD1XPWWNITuRBE0z1xaPp",
+        limit: "&limit=10",
+        rating: "&rating=pg",
         getSearch: function(term) {
-            var search = '&q=' + term;
+            var search = "&q=" + term;
             return search;
         }
-    }
-    
+    };
+
     var topicList = ["halloween", "fall", "beagle", "computers", "programming"];
-    
+
     function createButtons() {
-        $('header').empty();
-        topicList.forEach(function(t,i){
+        $("header").empty();
+        topicList.forEach(function(t, i) {
             var btn = $("<button>").addClass("gif-btn");
             btn.attr("data-index", i).text(t);
             $("header").append(btn);
         });
-        $(".gif-btn").on('click', clickButtons);
+        $(".gif-btn").on("click", clickButtons);
     }
-    
+
     function clickButtons() {
         var btnIndex = $(this).attr("data-index");
-        
+
         $.ajax({
-            url: queryURL.url + queryURL.getSearch(topicList[btnIndex]) + queryURL.limit + queryURL.rating,
+            url:
+                queryURL.url +
+                queryURL.getSearch(topicList[btnIndex]) +
+                queryURL.limit +
+                queryURL.rating,
             method: "GET"
         }).then(createGifs);
     }
-    
+
     function createGifs(gifData) {
-        $('section').empty();
+        $("section").empty();
         console.log(gifData);
-        for(var i = 0; i < gifData.data.length; i++) {
+        for (var i = 0; i < gifData.data.length; i++) {
             var gifDataItem = gifData.data[i];
-            var imgContainer = $('<div>').addClass('img-container');
-            var img = $('<img>').attr({
-                'src': gifDataItem.images.fixed_height_still.url,
-                'data-still': gifDataItem.images.fixed_height_still.url,
-                'data-ani': gifDataItem.images.fixed_height.url,
-                'data-state': 'still'
+            var imgContainer = $("<div>").addClass("img-container");
+            var img = $("<img>").attr({
+                src: gifDataItem.images.fixed_height_still.url,
+                "data-still": gifDataItem.images.fixed_height_still.url,
+                "data-ani": gifDataItem.images.fixed_height.url,
+                "data-state": "still"
             });
             var rating = gifDataItem.rating;
-            var pRating = $('<p>').addClass('rating').text('Rated: ' + rating.toUpperCase());
+            var pRating = $("<p>")
+                .addClass("rating")
+                .text("Rated: " + rating.toUpperCase());
 
             imgContainer.append(img).append(pRating);
             $("section").append(imgContainer);
         }
-        $('.img-container>img').on('click', switchImgState);
+        $(".img-container>img").on("click", switchImgState);
     }
-    
+
     function switchImgState() {
-        var state = $(this).attr('data-state');
-        if (state === 'still') {
-            var aniURL = $(this).attr('data-ani');
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            var aniURL = $(this).attr("data-ani");
             $(this).attr({
-                'src': aniURL,
-                'data-state': "ani"
-            }); 
+                src: aniURL,
+                "data-state": "ani"
+            });
         } else {
-            var stillURL = $(this).attr('data-still');
+            var stillURL = $(this).attr("data-still");
             $(this).attr({
-                'src': stillURL,
-                'data-state': 'still'
-            }); 
-            
+                src: stillURL,
+                "data-state": "still"
+            });
         }
     }
-    
-    $('input:text').focus(function() {
-        $(this).val('').attr('id', 'enter-search-topic');
+
+    $("input:text").focus(function() {
+        $(this)
+            .val("")
+            .attr("id", "enter-search-topic");
     });
 
-    $("form").on("submit", function( event ) {
+    $("input:text").focusout(function() {
+        $("input:text").val("Enter topic!").attr("id", "search-topic");
+    });
+
+    $("form").on("submit", function(event) {
         event.preventDefault();
-    
-        var topic = $('input:text').val().trim();
-        
-        if (topic != '' && topic != 'Enter topic!') {
+
+        var topic = $("input:text")
+            .val()
+            .trim();
+
+        if (
+            topic != "" &&
+            topic != "Enter topic!" &&
+            topicList.indexOf(topic) === -1
+        ) {
             topicList.push(topic);
         }
-        
-        $('input:text').val('Enter topic!').attr('id', 'search-topic');
+
+        $("input:text").val("");
         createButtons();
     });
 
